c2e424ff6d1ce8008a285dae6c32bc5d4f313830
Harianja Lundu <harianja@localhost.localdomain>
2022-05-28 07:34:30 +0700

    .gitignore watchlist/__init__.py watchlist/wsgi.py

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -1,8 +1,11 @@
+import os
 import requests
 import json
 
 from django.conf import settings
 from utils.utils import get_tmdb_configuration_url
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'watchlist.settings')
 
 def fetch_tmdb_configuration():
     resp = requests.get(get_tmdb_configuration_url())
