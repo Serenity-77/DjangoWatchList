8f0e4176c1f213d9c9b881c97f00cbff86046d59
Harianja Lundu <harianja@localhost.localdomain>
2022-05-28 08:17:46 +0700

    requirements.txt watchlist/settings.py

--- watchlist/settings.py
+++ watchlist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+3eet27na5xm3_t##-j1a^*s1sdpj3z$ivx*-*xp4tdsymiug4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "stormy-depths-39768.herokuapp.com", "0.0.0.0"]
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
